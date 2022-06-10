2a9d4ddde9cc7c642bd1f006b6b09abdf7bccdd5
Alexey956 <95160392+Alexey956@users.noreply.github.com>
2022-06-10 16:21:51 +0300

    Hello world

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace snake
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
